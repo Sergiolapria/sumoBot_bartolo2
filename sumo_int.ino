121451cea0bc1ba4824af2265d31178860e62d2f
Sergio <34064704+Sergiolapria@users.noreply.github.com>
2019-11-05 18:17:31 +0100

    Add files via upload

---
+++ sumo_int.ino
@@ -0,0 +1,85 @@
+
+#include <Wire.h>
+#include <VL53L0X.h>
+///PINES DE CONTROL DEL MOTOR
+#define IA1 10
+#define IB1 9
+#define IA2 12
+#define IB2 11
+#define pin_right 2
+#define pin_left 3
+//Haremos un control P de la distancia
+int distance_thresh1 = 552;
+int distance_thresh2=350;
+int distance_thresh3=100;
+int distance_thresh4=50;
+bool laser_status1=false;
+bool laser_status2=false;
+bool laser_status3=false;
+bool laser_status4=false;
+char laser_status=0;
+int t_push=10;
+////////
+int black_thresh=50;
+int distance = 0;
+boolean floor_right=false;
+boolean floor_left=false;
+boolean floor_status=false;
+VL53L0X sensor;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);//habilito serial para depuracion
+  //Configuracion de GPIO
+  setup_GPIO();
+  //habilita interrupciones
+  attachInterrupt(digitalPinToInterrupt(pin_right),ISR_izquierda,LOW);
+  attachInterrupt(digitalPinToInterrupt(pin_left),ISR_derecha,LOW);
+  sensor.init();
+  sensor.setTimeout(500);
+  
+  ///Vamos a establecer el modo de lectura continua
+  sensor.startContinuous();//si quisieramos establecer un intervalo de 100 ms hariamos
+  //sensor.startContinuous(100);
+  delay(5000);//retraso antes de comenzar de 5 sg
+}
+void setup_GPIO(){
+  pinMode(IA1, OUTPUT);
+  pinMode(IB1, OUTPUT);
+  pinMode(IA2, OUTPUT);
+  pinMode(IB2, OUTPUT);
+}
+void ISR_derecha(){
+  giro_izquierda(500);
+ 
+  Serial.println("INTERRUPCION DERECHA");
+}
+void ISR_izquierda(){
+  giro_derecha(500);
+  Serial.println("INTERRPCION IZQUIERDA");
+}
+void loop() {
+  scan_laser();
+   if(laser_status1==false && laser_status2==false && laser_status3==false && laser_status4==false){
+    giro_izquierda(20); 
+   }else{
+    ///CONTROL P
+      switch (laser_status){
+        case 'A':
+          adelante(t_push);
+        break;
+        case 'B':
+          adelante(2*t_push);
+        break;
+        case 'C':
+          adelante(3*t_push);
+        break;
+        case 'D':
+          adelante(4*t_push);
+        break;
+        case 'E':
+          adelante(5*t_push);
+        break;
+      }
+   }
+ }
